76ab298bec39a3b731b667c7158d9056fb34673a
Danh Thai <ronwindy@gmail.com>
2021-10-04 22:49:50 +0700

    finish slideshow

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -1,16 +1,27 @@
 import "./Slideshow.scss";
 
+import { Link } from "react-router-dom";
+
 function Slideshow() {
   return (
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/5lVlhbEcAv0"
-      title="YouTube video player"
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    ></iframe>
+    <div className="slideshow-container">
+      <iframe
+        src="https://www.youtube.com/embed/5lVlhbEcAv0"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      <div className="trendy-btn-container">
+        <Link
+          className="trendy_btn button_two radius shadow"
+          to="/slideshow"
+          title="Button Two"
+        >
+          <span className="txt">Xem xong click đây nghen 😘</span>
+        </Link>
+      </div>
+    </div>
   );
 }
 
